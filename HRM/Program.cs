da240ccf987450f5a3374e7052b69025c195619b
abirChowdhry <abirchowdhury737@gmail.com>
2026-05-27 04:48:01 +1000

    Frontend Port Issue fixed

--- HRM/Program.cs
+++ HRM/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("HrmClient", policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "http://127.0.0.1:5173")
+        policy.WithOrigins("http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
